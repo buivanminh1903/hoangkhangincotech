326e56d4cfa4f4b9bdcf03fcc5bdf35a8b7374fe
lvthanh0203 <101349080+lvthanh0203@users.noreply.github.com>
2022-03-24 23:25:03 +0700

    Thanh Luong upload

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class IndexController extends Controller
+{
+    public function index() {
+        return view('index', [
+            'title' => 'Hoàng Khang Incotech'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+/*Trang chu*/
+Route::get('/', [\App\Http\Controllers\IndexController::class, 'index']);
 
 /*Danh sách dịch vụ*/
 Route::get('dich-vu', [\App\Http\Controllers\ServiceController::class, 'index']);
