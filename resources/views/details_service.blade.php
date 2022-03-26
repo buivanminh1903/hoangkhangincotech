2b653ffad47a97703a2e029f1ac7ba1f3c8feb0f
TQuannn <1800002809@nttu.edu.vn>
2022-03-26 14:31:11 +0700

    quanupdate

--- resources/views/details_service.blade.php
+++ resources/views/details_service.blade.php
@@ -20,39 +20,33 @@
         <h1 class="service-title"> Mobile Apps Development</h1>
         <img class="serice-image" src="image/banner.png" style="width: 100%;" alt="">
         <div class="service-title-open">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac eros a tellus posuere gravida. Vestibulum
-            eget sapien eleifend, imperdiet orci nec, venenatis libero. Sed sit amet turpis nec ipsum condimentum
-            sagittis non vel massa. Ut dapibus fermentum
-            nibh, eget placerat urna viverra vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-            per inceptos himenaeos. Mauris nec nisl aliquam, porttitor leo ac, pharetra metus. Sed ultrices blandit
-            ipsum vitae faucibus. Nunc
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac eros a tellus posuere gravida. Vestibulum eget sapien eleifend, imperdiet orci nec, venenatis libero. Sed sit amet turpis nec ipsum condimentum sagittis non vel massa. Ut dapibus fermentum
+            nibh, eget placerat urna viverra vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris nec nisl aliquam, porttitor leo ac, pharetra metus. Sed ultrices blandit ipsum vitae faucibus. Nunc
             tristique convallis tristique.
         </div>
         <div class="service-content">
-            Bibendum nunc vel velit pharetra, eu bibendum sapien tincidunt. Etiam laoreet ultricies dolor in pharetra.
-            Sed gravida eros sed velit bibendum, laoreet lobortis ligula tincidunt. Integer gravida auctor hendrerit.
+            Bibendum nunc vel velit pharetra, eu bibendum sapien tincidunt. Etiam laoreet ultricies dolor in pharetra. Sed gravida eros sed velit bibendum, laoreet lobortis ligula tincidunt. Integer gravida auctor hendrerit.
         </div>
         <div class="service-content-image">
             <h3>Key features</h3>
             <ul>
-                <li><img src="image/iconcheck.png" alt=""> <span> Custom installtion Process</span></li>
-                <li><img src="image/iconcheck.png" alt=""> Custtomizable All Performance</li>
-                <li><img src="image/iconcheck.png" alt=""> Imprved All Performance</li>
-                <li><img src="image/iconcheck.png" alt=""> High Usablitily</li>
+                <li> <img src="image/iconcheck.png" alt=""> <span> Custom installtion Process</li>
+                <li> <img src="image/iconcheck.png" alt=""> Custtomizable All Performance</li>
+                <li> <img src="image/iconcheck.png" alt=""> Imprved All Performance</li>
+                <li> <img src="image/iconcheck.png" alt=""> High Usablitily</li>
             </ul>
         </div>
     </div>
-    <div class="service-main">
-        <div class="container" style="width: 1170px;">
+    <div class="service-main" >
+        <div class="container" style="width: 1170px;" >
             <h3 class="title"> Pricing Plan</h3>
-            <div class="row">
+            <div class="row" >
                 <div class="col-md-4 col-sm-12 ">
                     <div class="service-main-box">
                         <p class="service-main-box1">PRICING TABLE</p>
                         <p class="service-main-box2">PREMIUM</p>
                         <div class="box">
-                            <p class="service-main-box3" style="font-weight: 400"><sup style="font-size:13px;">$</sup>199
-                            </p>
+                            <p class="service-main-box3"style="font-weight: 400"><sup style="font-size:13px;">$</sup>199</p>
                             <p class="service-main-box4" style="color: #888787">Per Month</p>
                             </p>
                         </div>
@@ -60,13 +54,9 @@
                             <ul>
                                 <li><img src="image/iconcheckk.png" alt="">Conpect Development</li>
                                 <li><img src="image/iconcheckk.png" alt="">UI/UX Design</li>
-                                <li class="service_text_dark"><img src="image/iconcheckmo.png" alt="">Configuration
-                                    Management
-                                </li>
-                                <li class="service_text_dark"><img src="image/iconcheckmo.png" alt="">App intergition
-                                </li>
-                                <li class="service_text_dark"><img src="image/iconcheckmo.png" alt="">App Intergition
-                                </li>
+                                <li class="service_text_dark"><img src="image/iconcheckmo.png" alt="">Configuration Management</li>
+                                <li class="service_text_dark"><img src="image/iconcheckmo.png" alt="">App intergition</li>
+                                <li class="service_text_dark"><img src="image/iconcheckmo.png" alt="">App Intergition</li>
                             </ul>
 
                         </div>
@@ -79,8 +69,7 @@
                         <p class="service-main-box1">PRICING TABLE</p>
                         <p class="service-main-box2">BASIC</p>
                         <div class="box">
-                            <p class="service-main-box3" style="font-weight: 400"><sup style="font-size:13px;">$</sup>199
-                            </p>
+                            <p class="service-main-box3"style="font-weight: 400"><sup style="font-size:13px;">$</sup>199</p>
                             <p class="service-main-box4" style="color: #888787">Per Month</p>
                         </div>
                         <div class="service-main-box5">
@@ -88,10 +77,8 @@
                                 <li><img src="image/iconcheckk.png" alt="">Conpect Development</li>
                                 <li><img src="image/iconcheckk.png" alt="">UI/UX Design</li>
                                 <li><img src="image/iconcheckk.png" alt="">Configuration Management</li>
-                                <li class="service_text_dark"><img src="image/iconcheckmo.png" alt="">App intergition
-                                </li>
-                                <li class="service_text_dark"><img src="image/iconcheckmo.png" alt="">App Intergition
-                                </li>
+                                <li class="service_text_dark"><img src="image/iconcheckmo.png" alt="">App intergition</li>
+                                <li class="service_text_dark"><img src="image/iconcheckmo.png" alt="">App Intergition</li>
                             </ul>
 
                         </div>
@@ -105,17 +92,16 @@
                         <p class="service-main-box1">PRICING TABLE</p>
                         <p class="service-main-box2">GOLDEN</p>
                         <div class="box">
-                            <p class="service-main-box3" style="font-weight: 400"><sup style="font-size:13px;">$</sup>199
-                            </p>
+                            <p class="service-main-box3"style="font-weight: 400"><sup style="font-size:13px;">$</sup>199</p>
                             <p class="service-main-box4" style="color: #888787 ;">Per Month</p>
                         </div>
                         <div class="service-main-box5">
                             <ul>
-                                <li><img src="image/iconcheckk.png" alt="">Conpect Development</li>
+                                <li> <img src="image/iconcheckk.png" alt="">Conpect Development</li>
                                 <li><img src="image/iconcheckk.png" alt="">UI/UX Design</li>
-                                <li><img src="image/iconcheckk.png" alt="">Configuration Management</li>
-                                <li><img src="image/iconcheckk.png" alt="">App intergition</li>
-                                <li><img src="image/iconcheckk.png" alt="">App Intergition</li>
+                                <li ><img src="image/iconcheckk.png" alt="">Configuration Management</li>
+                                <li ><img src="image/iconcheckk.png" alt="">App intergition</li>
+                                <li ><img src="image/iconcheckk.png" alt="">App Intergition</li>
                             </ul>
 
                         </div>
@@ -127,7 +113,7 @@
             </div>
         </div>
     </div>
-    <div class="all-service">
+    <div class="all-service" >
         <div>
             <h3 class="all-service-title">All Services</h3>
             <div class="container" style="width: 1170px;">
@@ -136,63 +122,61 @@
                     <div class="row">
 
                         <div class="col-md-4 col-6">
-                            <div class="content">
+                            <div class="contentt">
                                 <h4>Digital Marketing</h4>
-                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos
-                                    techno</p>
+                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos techno</p>
                             </div>
                             <img src="image/allservice.png" alt="">
                             <a href=""><i style="margin: auto;" class="fa-solid fa-angle-right"></i></a>
 
                         </div>
                         <div class="col-md-4 col-6">
-                            <div class="content">
+                            <div class="contentt">
                                 <h4>Digital Marketing</h4>
-                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos
-                                    techno</p>
+                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos techno</p>
                                 <img src="image/allservice.png" alt="">
                                 <a href=""><i style="margin: auto;" class="fa-solid fa-angle-right"></i></a>
                             </div>
                         </div>
                         <div class="col-md-4 col-6">
-                            <div class="content">
+                            <div class="contentt">
                                 <h4>Digital Marketing</h4>
-                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos
-                                    techno</p>
+                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos techno</p>
                             </div>
                             <img src="image/allservice1.png" alt="">
                             <a href=""><i class="fa-solid fa-angle-right" style="margin: auto;"></i></a>
                         </div>
                         <div class="col-md-4 col-6">
-                            <div class="content">
+                            <div class="contentt">
                                 <h4>Digital Marketing</h4>
-                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos
-                                    techno</p>
+                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos techno</p>
                             </div>
                             <img src="image/allservice2.png" alt="">
                             <a href=""><i style="margin: auto;" class="fa-solid fa-angle-right"></i></a>
                         </div>
                         <div class="col-md-4 col-6">
-                            <div class="content">
+                            <div class="contentt">
                                 <h4>Digital Marketing</h4>
-                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos
-                                    techno</p>
+                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos techno</p>
                             </div>
                             <img src="image/allservice3.png" alt="">
                             <a href=""><i style="margin: auto;" class="fa-solid fa-angle-right"></i></a>
                         </div>
                         <div class="col-md-4 col-6">
-                            <div class="content">
+                            <div class="contentt">
                                 <h4>Digital Marketing</h4>
-                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos
-                                    techno</p>
+                                <p>Conveniment predomiate sticky aligmi than lơw risk high methods of emp brand supeinos techno</p>
                             </div>
                             <img src="image/allservice1.png" alt="">
                             <a href=""><i style="margin: auto;" class="fa-solid fa-angle-right"></i></a>
                         </div>
                     </div>
                 </div>
+
+
             </div>
         </div>
+
+
     </div>
 @endsection
