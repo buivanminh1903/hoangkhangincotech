1b28cc28175fe6695749ba363923342e0eee79bf
phuc29052000 <phuc29052000@gmail.com>
2022-04-05 16:22:18 +0700

    Phuc Ho fix

--- app/Models/contactus.php
+++ app/Models/contactus.php
@@ -16,11 +16,7 @@ class contactus extends Model
     
    protected $primarykey = 'id';
    
-<<<<<<< HEAD
-   protected $fillable = ['subject', 'messages', 'name' , 'email' , 'phone'];
-=======
    protected $fillable = ['subject', 'messages', 'names' , 'email' , 'phone' ,'position'];
->>>>>>> f67e7f3c58ccfd9193e4a733ddace564b5f8dcb3
 
    public function getAllContactus(){
 
